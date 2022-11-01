9b84570635854ff89b4c1d7b5a972d28df9584d1
nandk4552 <nandk4552@gmail.com>
2022-11-01 21:16:58 +0530

    initial commit

---
+++ components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+    BellIcon,
+    HashtagIcon,
+    BookmarkIcon,
+    UserIcon,
+    HomeIcon,
+    QuestionMarkCircleIcon,
+    AtSymbolIcon,
+    EnvelopeIcon,
+    InboxIcon,
+    EllipsisHorizontalCircleIcon
+} from '@heroicons/react/24/outline'
+import SidebarRow from './SidebarRow'
+
+const Sidebar = () => {
+    return (
+        <div className='flex flex-col'>
+            <img className='h-10 w-10' src="https://links.papareact.com/drq" alt="" />
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={HashtagIcon} title="Explore" />
+            <SidebarRow Icon={BellIcon} title="Notifications" />
+            <SidebarRow Icon={EnvelopeIcon} title="Messages" />
+            <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+            <SidebarRow Icon={InboxIcon} title="Lists" />
+            <SidebarRow Icon={UserIcon} title="Sign In" />
+            <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
+
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ sanity/schemas/tweet.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'tweet',
+  title: 'Tweet',
+  type: 'document',
+  fields: [
+    {
+      name: 'text',
+      title: 'Text in Tweet',
+      type: 'string',
+    },
+    {
+      name: 'blockTweet',
+      title: 'Block Tweet',
+      description: 'ADMIN controls: Toggle is deemed inappropriate',
+      type: 'boolean',
+    },
+    {
+      name: 'username',
+      title: 'Username',
+      type: 'string',
+    },
+    {
+      name: 'profileImg',
+      title: 'Profile Image',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Tweet Image',
+      type: 'string',
+    },
+
+  ],
+
+
+}
